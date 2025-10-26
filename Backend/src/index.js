b2019c703e9d4665aed14791aa01b89734e36892
PARTHKATARIA22 <katariaparth222@gmail.com>
2025-10-26 23:10:08 +0530

    Deployment issue corrected

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,10 +21,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    app.get("/*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
     })
 }
 
